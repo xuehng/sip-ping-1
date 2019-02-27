090e3937702c4f7cb5490dd8ff9d6d4b23590f5a
xrh <rihong@taillkang.com>
2019-02-27 19:11:08 +0800

    命令行：sip-ping -addr sip://192.168.0.15:5060

--- main.go
+++ main.go
@@ -1,176 +1,126 @@
 package main
 
 import (
-  "bufio"
-  "crypto/rand"
-  "crypto/tls"
-  "flag"
-  "fmt"
-  "log"
-  "os"
-  "os/signal"
-  "strings"
-  "strconv"
-  "time"
-  "net/url"
-  "net"
-  mathrand "math/rand"
+	"bufio"
+	"bytes"
 
-  "github.com/gorilla/websocket"
+	"flag"
+	"fmt"
+	"log"
+	mathrand "math/rand"
+	"net"
+	"net/url"
+	"os"
+
+	"strings"
+
+	"text/template"
+	"time"
 )
+
+type SipOption struct {
+	Url, Localhost, Localport string
+	Callid, Cseq, Branchrand  int
+}
+
 var addr = flag.String("addr", "", "http service address")
 var skipVerify = flag.Bool("skipverify", false, "skip TLS certificate verification")
 
-const WS_OPTIONS = `OPTIONS sip:monitor@none SIP/2.0
-Via: SIP/2.0/WSS 81okseq92jb7.invalid;branch=z9hG4bK5964427
-To: <sip:ba_user@none>
-From: <sip:anonymous.8scs48@anonymous.invalid>;tag=fql2c8mlg3
-Call-ID: {{callId}}
-CSeq: {{seq}} OPTIONS
-Content-Length: 0
-
-` // two newlines required to signal end of request
-
-const TCP_OPTIONS = `OPTIONS sip:host@invalid:1738;transport=tcp SIP/2.0
-Via: SIP/2.0/TCP 127.0.0.1:5066;branch=z9hG4bKr1t13cmvZDjtg
+const UDP_OPTIONS = `OPTIONS {{.Url}};transport=udp SIP/2.0
+Call-ID: {{.Callid}}@{{.Localhost}}
+CSeq: {{.Cseq}} OPTIONS
 Max-Forwards: 70
-From: "" <sip:monitor@invalid>
-To: <sip:host@invalid;transport=tcp>
-Call-ID: {{callId}}
-CSeq: {{seq}} OPTIONS
+From: SIP-PING <sip:sip-ping@invalid>;tag=00-01991-00116239-25a473e47
+To: <sip:host@invalid;transport=udp>
+Via: SIP/2.0/UDP {{.Localhost}}:{{.Localport}};branch=z9hG4bK{{.Branchrand}}
+Accept: application/sdp
 Content-Length: 0
 
 ` // two newlines required to signal end of request
 
-func randString(n int) string {
-    const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    var bytes = make([]byte, n)
-    rand.Read(bytes)
-    for i, b := range bytes {
-        bytes[i] = alphanum[b % byte(len(alphanum))]
-    }
-    return string(bytes)
-}
+func renderRequest(options string, url string, localaddr string) *bytes.Buffer {
+	localAddrInfos := strings.Split(localaddr, ":")
+	if len(localAddrInfos) < 2 {
+		return nil
+	}
 
-func renderRequest(options string) string {
-    mathrand.Seed(time.Now().UnixNano())
-    req := strings.Replace(options, "{{callId}}", randString(20), -1)
-    req = strings.Replace(req, "{{seq}}", strconv.Itoa(mathrand.Intn(99999)), -1)
-    req = strings.Replace(req, "\n", "\r\n", -1)
-    return req
+	mathrand.Seed(time.Now().UnixNano())
+
+	data := SipOption{
+		Url:        url,
+		Localhost:  localAddrInfos[0],
+		Localport:  localAddrInfos[1],
+		Callid:     mathrand.Intn(1<<31 - 1),
+		Cseq:       mathrand.Intn(1<<31 - 1),
+		Branchrand: mathrand.Intn(1<<31 - 1),
+	}
+	tmp, err := template.New("name1").Parse(options)
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	var buf bytes.Buffer
+	if err = tmp.Execute(&buf, data); err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	return &buf
 }
 
 func main() {
-  flag.Parse()
-  log.SetFlags(0)
+	flag.Parse()
+	log.SetFlags(0)
 
-  if (len(*addr)==0) {
-    flag.Usage()
-    os.Exit(255)
-  }
+	if len(*addr) == 0 {
+		flag.Usage()
+		os.Exit(255)
+	}
 
-  var url, err = url.Parse(*addr)
-  if err != nil {
-    log.Fatal("addr:", err)
-  }
+	var url, err = url.Parse(*addr)
+	if err != nil {
+		log.Fatal("addr:", err)
+	}
 
-  if (url.Scheme == "wss" || url.Scheme == "ws") {
+	if url.Scheme == "sip" {
+		conn, err := net.DialTimeout("udp", url.Host, (time.Duration(5) * time.Second))
+		if err != nil {
+			log.Fatal("dial:", err)
+			return
+		}
+		defer conn.Close()
 
-    var tlsClientConfig = &tls.Config{InsecureSkipVerify: *skipVerify}
-    var sipDialer = websocket.Dialer{
-      Subprotocols:    []string{"sip"},
-      ReadBufferSize:  1024,
-      WriteBufferSize: 1024,
-      TLSClientConfig: tlsClientConfig,
-    }
+		buf := renderRequest(UDP_OPTIONS, *addr, conn.LocalAddr().String())
+		if buf == nil {
+			log.Println("render request fail")
+			return
+		}
 
-    interrupt := make(chan os.Signal, 1)
-    signal.Notify(interrupt, os.Interrupt)
+		fmt.Println(buf)
 
-    c, _, err := sipDialer.Dial(*addr, nil)
-    if err != nil {
-      log.Fatal("dial:", err)
-    }
+		conn.SetDeadline(time.Now().Add(time.Duration(5) * time.Second))
 
-    defer c.Close()
+		if _, err = conn.Write(buf.Bytes()); err != nil {
+			log.Println("send request fail")
+			return
+		}
 
-    done := make(chan struct{})
+		reader := bufio.NewReader(conn)
+		scanner := bufio.NewScanner(reader)
+		scanner.Split(bufio.ScanLines)
 
-    go func() {
-      defer c.Close()
-      defer close(done)
-      for {
-        _, message, err := c.ReadMessage()
-        if err != nil {
-          log.Println("read:", err)
-          os.Exit(1)
-          return
-        }
-        log.Printf("%s", message)
-        if (strings.Contains(string(message), "SIP/2.0 200 OK")) {
-          os.Exit(0)
-        } else {
-          os.Exit(1)
-        }
-      }
-    }()
+		var ok200 bool
 
-    req := renderRequest(WS_OPTIONS)
+		for noErr := scanner.Scan(); noErr == true && scanner.Text() != ""; scanner.Scan() {
+			fmt.Println(scanner.Text()) // scanner.Bytes()
 
-    err = c.WriteMessage(websocket.TextMessage, []byte(req))
-    if err != nil {
-      log.Println("write err:", err)
-      return
-    }
-    log.Println("======================")
-    log.Println(req)
-    log.Println("======================")
+			if strings.Contains(scanner.Text(), "SIP/2.0 200 OK") {
+				ok200 = true
+			}
+		}
 
-    for {
-      select {
-      case <-time.After(15*time.Second):
-        log.Println("read timeout")
-        os.Exit(1)
-      case <-interrupt:
-        log.Println("interrupt")
-        // To cleanly close a connection, a client should send a close
-        // frame and wait for the server to close the connection.
-        err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-        if err != nil {
-          log.Println("write close:", err)
-          return
-        }
-        select {
-        case <-done:
-        }
-        c.Close()
-        return
-      }
-    }
-  } else if (url.Scheme == "sip" || url.Scheme == "tcp") {
-    conn, err := net.Dial("tcp", url.Host)
-    if err != nil {
-      log.Fatal("dial:", err)
-    }
-    req := renderRequest(TCP_OPTIONS)
-    log.Println("======================")
-    log.Println(req)
-    log.Println("======================")
-    fmt.Fprintf(conn, req)
-    status, err := bufio.NewReader(conn).ReadString('\n')
-    if err != nil {
-      log.Fatal("read:", err)
-      os.Exit(1)
-    }
-    log.Println(status)
-    if (strings.Contains(status, "SIP/2.0 200 OK")) {
-      os.Exit(0)
-    } else {
-      os.Exit(1)
-    }
-  } else {
-    log.Println("Unknown scheme:", url.Scheme);
-    os.Exit(255)
-  }
+		fmt.Print("reachable: ", ok200)
+	} else {
+		log.Println("Unknown scheme:", url.Scheme)
+		os.Exit(255)
+	}
 }
-
